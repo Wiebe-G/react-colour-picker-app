bfa6876a5c80a399c5a1261f43090a6ab3bfe55e
Wiebe Gouma <goumawiebe1234@proton.me>
2026-03-17 09:57:48 +0100

    Styling

--- src/ColourPicker.tsx
+++ src/ColourPicker.tsx
@@ -1,23 +1,33 @@
 import {useState} from "react";
 
-function ColourPicker(){
+function ColourPicker() {
 
 	const [colour, setColour] = useState("#FFFFFF");
 
-	function handleColourChange(_e: React.ChangeEvent<HTMLInputElement>){
+	function handleColourChange(_e: React.ChangeEvent<HTMLInputElement>) {
 		setColour(_e.target.value);
 	}
 
-	return(
+	function resetColour(): void {
+		setColour("#FFFFFF");
+	}
+
+	return (
 		<>
-			<div className="flex flex-col items-center">
-				<h1>Colour picker</h1>
-				<div className="border-2 border-black" style={{backgroundColor: colour}}>
-					<p>Selected colour: {colour}</p>
+			<div className="flex justify-center h-screen m-2">
+				<div className="flex flex-col items-center gap-1 border-2 border-gray-800 w-1/2 h-1/2">
+					<h1 className="font-bold">Colour picker</h1>
+					<div className="flex justify-center items-center border-2 border-black h-1/2 w-2xl" style={{backgroundColor: colour}}>
+						<p>Selected colour: {colour}</p>
+					</div>
+					<label>Select a colour</label>
+					<input type="color" value={colour} onChange={handleColourChange}/>
+					<button onClick={resetColour}
+					        className="btn btn-primary rounded-xl bg-blue-500 text-white my-2 py-2 border-2 border-solid border-black">
+						Reset
+						colour
+					</button>
 				</div>
-				<label>Select a colour</label>
-				<input type="color" value={colour} onChange={handleColourChange}/>
-				<button></button>
 			</div>
 		</>
 	);
